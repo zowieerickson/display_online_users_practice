73fc9124644ef50bc1b6da8de78aaf6bc87067c6
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-29 13:46:31 -0400

    fetching users from users.json, currently displaying first user to test, added basic styles

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi zowie');
+async function findUsers() {
+    const response = await fetch('users.json');
+    const data = await response.json();
+    return data;
+}
+
+findUsers().then(data => {
+    const testUser = data[0];
+    console.log(testUser);
+    const html = `
+    <div class="user-container">
+        <div class="user">
+            <p class="user-name">${testUser.username}</p>
+            <div class="user-online"></div>
+        </div>
+    </div>
+    `;
+
+    document.querySelector('main').innerHTML = html;
+})
